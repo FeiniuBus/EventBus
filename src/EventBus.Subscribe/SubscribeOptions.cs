f2090d28a81930a21e7f425f7bf4b9c9b64ff907
hongyanshen <shy@feiniubus.com>
2017-09-05 17:04:25 +0800

    rm ctor

--- src/EventBus.Subscribe/SubscribeOptions.cs
+++ src/EventBus.Subscribe/SubscribeOptions.cs
@@ -8,11 +8,6 @@ namespace EventBus.Subscribe
 {
     public class SubscribeOptions
     {
-        public SubscribeOptions()
-        {
-
-        }
-
         public string DefaultGroup { get; set; }
 
         public int ConsumerClientCount { get; set; } = 1;
@@ -20,6 +15,22 @@ namespace EventBus.Subscribe
         public string DefaultExchange => "default.exchange@feiniubus";
 
         public IList<SubscribeInfo> SubscribeInfos { get; } = new List<SubscribeInfo>();
+
+        public void RegisterExternalCallback(string topic, Type callbackType)
+        {
+            if (SubscribeInfos.Any(x => x.Topic == topic && x.Group == DefaultGroup))
+            {
+                throw new InvalidOperationException($"Duplicated subscribe topic={topic} group={DefaultGroup}");
+            }
+
+            SubscribeInfos.Add(new SubscribeInfo
+            {
+                Exchange = DefaultExchange,
+                Topic = topic,
+                Group = DefaultGroup,
+                CallbackType = callbackType
+            });
+        }
 
         public void RegisterExternalCallback(string topic, string group, Type callbackType)
         {
