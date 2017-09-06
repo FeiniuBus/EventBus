320977fa61e2d4cd4c03fc9532be51350f17dd78
andrew.cui <mykge@outlook.com>
2017-09-06 10:00:42 +0800

    ReceivedEventPersistenter

---
+++ src/EventBus.Subscribe/Internal/Model/ReceivedMessage.cs
@@ -0,0 +1,20 @@
+﻿using EventBus.Core.State;
+using System;
+
+namespace EventBus.Subscribe.Internal.Model
+{
+    public class ReceivedMessage
+    {
+        public long Id { get; set; }
+        public long MessageId { get; set; }
+        public long TransactId { get; set; }
+        public string Group { get; set; }
+        public string RouteKey { get; set; }
+        public string MetaData { get; set; }
+        public string Content { get; set; }
+        public int Retries { get; set; }
+        public DateTime ReceivedTime { get; set; }
+        public DateTime ExpiredTime { get; set; }
+        public MessageState State { get; set; }
+    }
+}
