0bd4d224226d5ea7adaf3afa8d48aeaa38c48dc0
andrew.cui <mykge@outlook.com>
2017-09-06 11:20:53 +0800

    add ado.net publish method

--- src/EventBus.Core/IEventPublisher.Extensions.cs
+++ src/EventBus.Core/IEventPublisher.Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using EventBus.Core.Infrastructure;
 using EventBus.Core.Internal;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,6 +27,27 @@ namespace EventBus.Core
             }
 
             await eventPublisher.PrepareAsync(new EventDescriptor(routeKey, message, exchange, args));
+        }
+
+        public static async Task PrepareAsync(this IEventPublisher eventPublisher, string routeKey, object content, object metaData, IDbConnection dbConnection, IDbTransaction dbTransaction, string exchange = "default.exchange@feiniubus", object args = null)
+        {
+            var message = new DefaultMessage(content);
+
+            var anonyObj = new AnonymousObject(metaData);
+            var anonyMembers = anonyObj.GetProperties().Where(member => member.MemberType == System.Reflection.MemberTypes.Property)
+                .Select(member => new { MemberName = member.MemberName, Value = anonyObj.GetValue(member.DeclaringType, member.MemberName) });
+
+            if (anonyMembers.Any())
+            {
+                var md = new MetaData();
+                foreach (var member in anonyMembers)
+                {
+                    md.Set(member.MemberName, member.Value.ToString());
+                }
+                message.MetaData.Contact(md);
+            }
+
+            await eventPublisher.PrepareAsync(new EventDescriptor(routeKey, message, exchange, args), dbConnection,dbTransaction);
         }
     }
 }
