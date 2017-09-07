e0d84ab0ab0fb8bb3cb555d213e5cb73c9c5e52d
徐乔伦 <xql@feiniubus.com>
2017-09-07 14:00:39 +0800

    delete unused script

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-dotnet restore ../src/EventBus.Core/EventBus.Core.csproj -v m
-dotnet build ../src/EventBus.Core/EventBus.Core.csproj -v m
-
-dotnet restore ../src/EventBus.Publish/EventBus.Publish.csproj -v m
-dotnet build ../src/EventBus.Publish/EventBus.Publish.csproj -v m
-
-dotnet restore ../src/EventBus.Subscribe/EventBus.Subscribe.csproj -v m
-dotnet build ../src/EventBus.Subscribe/EventBus.Subscribe.csproj -v m
