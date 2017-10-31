e7756a683e9fcc568477e2b7eb47ee1f897a1a2e
Andrew <mykge@outlook.com>
2017-10-31 17:36:58 +0800

    upgrade project version

--- EventBus.NuGet.Sample/Startup.cs
+++ EventBus.NuGet.Sample/Startup.cs
@@ -1,37 +1,26 @@
-﻿using EventBus.Sample.EventHandlers;
+﻿using EventBus.Alert;
+using EventBus.Core;
 using EventBus.Subscribe;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using EventBus.Sample.FailedEventHandlers;
-using EventBus.Core;
-using EventBus.Alert;
 
-namespace EventBus.Sample
+namespace EventBus.NuGet.Sample
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
-
+        public IConfiguration Configuration { get; }
         private const string ConnectionString = "Server=192.168.126.138;Port=3306;Database=FeiniuCAP; User=root;Password=kge2001;charset=UTF-8";
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
             services.AddMvc();
 
             services.AddDbContext<SampleDbContext>(options => options.UseMySql(ConnectionString));
@@ -49,7 +38,7 @@ namespace EventBus.Sample
 
                 options.UseFailureHandle(failure =>
                 {
-                    failure.RegisterFailureCallback("eventbus.testtopic", typeof(NewUserFailedMessageHandler));
+                    
                 });
             });
 
@@ -58,9 +47,7 @@ namespace EventBus.Sample
             {
                 options.ConsumerClientCount = 5;
                 options.DefaultGroup = "FeiniuBusPayment1111";
-
-                options.RegisterCallback("eventbus.testtopic", "Hello-World", typeof(NewUserEventHandler));
-                options.RegisterCallback("charge.success", typeof(NewUserEventHandler));
+                
                 //options.RegisterCallback("eventbus.testtopic", "eventbus.testgroup2", typeof(NewUserEventHandler));
             });
 
@@ -70,14 +57,16 @@ namespace EventBus.Sample
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddDebug()
-                .AddConsole(LogLevel.Information);
-
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseEventBus();
-
             app.UseMvc();
+
+
         }
     }
 
